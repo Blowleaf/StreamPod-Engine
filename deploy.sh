657be5188781178d4752ac5d06e9cdfd8cb2bef5
Octavio Cano <tavikano@gmail.com>
2023-10-06 16:20:54 +0900

    fix(metadata updates):updates in deploy.sh to fix port 1935

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ kubectl apply -f ./streampod-engine-deployment.yml
 # Expose the main containers
 kubectl expose deployment/streampod-engine \
         --port=8080 --target-port=8080 \
-        --port=1953 --target-port=1935 \
+        --port=1935 --target-port=1935 \
         --name=streampod-engine-service --type=LoadBalancer
 
 # Get services
